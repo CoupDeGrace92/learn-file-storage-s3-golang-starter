038f690b8ed470140e7565094e464f50b2d31a28
Nick <nickoboy1992@gmail.com>
2026-01-08 15:19:11 -0600

    Updated image storage to store raw image data in the db instead of a map that reset with the server

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20 //this gives us 10mbs since bit shifting 10 << 20 is the same as 10 * 1024 * 1024
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse multipart form", err)
+		return
+	}
+	File, multiHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't form file", err)
+		return
+	}
+	defer File.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	fileType := multiHeader.Header.Get("Content-Type")
+
+	imageBytes, err := io.ReadAll(File)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error converting multiFile to bytes", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error retrieving video from databse", err)
+		return
+	}
+	if video.UserID != userID {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	encodedImage := base64.StdEncoding.EncodeToString(imageBytes)
+	encodedImageURL := fmt.Sprintf("data:%s;base64,%s", fileType, encodedImage)
+
+	video.ThumbnailURL = &encodedImageURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating internal db with new thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
